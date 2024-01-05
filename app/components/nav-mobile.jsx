48a745dcc7f5e3fd62af28dbdf897c50dd665c72
Shane Murphy <shn.murphy@gmail.com>
2024-01-05 13:25:48 +0000

    Updated Mobile nav

--- app/components/nav-mobile.jsx
+++ app/components/nav-mobile.jsx
@@ -1,7 +1,7 @@
 export default function NavBarMobile() {
   return (
     <div className="w-11/12 mx-auto">
-      <div className="absolute w-11/12 z-10 flex bg-white mt-4 px-3 justify-between items-center rounded-lg mobile-nav">
+      <div className="absolute w-11/12 z-10 flex bg-white mt-12 px-3 py-1 justify-between items-center rounded-lg mobile-nav">
         <div className="font-bold text-xl">Logo</div>
         <div>
           <div className="text-black p-2">Menu</div>
