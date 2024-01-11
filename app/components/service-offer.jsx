6fef6cb20252f9b80d4775b40bd7919fa7b97239
Shane Murphy <shn.murphy@gmail.com>
2024-01-11 15:26:34 +0000

    Build issue with Service Offer

--- app/components/service-offer.jsx
+++ app/components/service-offer.jsx
@@ -2,11 +2,14 @@ export default function ServiceOffer() {
   return (
     <div>
       <div className="flex-col overflow-hidden relative flex min-h-[611px] pl-14 pr-20 py-12 items-start max-md:max-w-full max-md:px-5">
-        <img
-          loading="lazy"
-          srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/3bf66373c66580082c772d0565f1476db76e3eec2c3bcdd4f5c403ab471f75a7?apiKey=052aa92796d54d509e3efd8cbd8b7989&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/3bf66373c66580082c772d0565f1476db76e3eec2c3bcdd4f5c403ab471f75a7?apiKey=052aa92796d54d509e3efd8cbd8b7989&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/3bf66373c66580082c772d0565f1476db76e3eec2c3bcdd4f5c403ab471f75a7?apiKey=052aa92796d54d509e3efd8cbd8b7989&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/3bf66373c66580082c772d0565f1476db76e3eec2c3bcdd4f5c403ab471f75a7?apiKey=052aa92796d54d509e3efd8cbd8b7989&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/3bf66373c66580082c772d0565f1476db76e3eec2c3bcdd4f5c403ab471f75a7?apiKey=052aa92796d54d509e3efd8cbd8b7989&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/3bf66373c66580082c772d0565f1476db76e3eec2c3bcdd4f5c403ab471f75a7?apiKey=052aa92796d54d509e3efd8cbd8b7989&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/3bf66373c66580082c772d0565f1476db76e3eec2c3bcdd4f5c403ab471f75a7?apiKey=052aa92796d54d509e3efd8cbd8b7989&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/3bf66373c66580082c772d0565f1476db76e3eec2c3bcdd4f5c403ab471f75a7?apiKey=052aa92796d54d509e3efd8cbd8b7989&"
-          className="absolute h-full w-full object-cover object-center inset-0"
-        />
+        <picture>
+          <img
+            loading="lazy"
+            srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/3bf66373c66580082c772d0565f1476db76e3eec2c3bcdd4f5c403ab471f75a7?apiKey=052aa92796d54d509e3efd8cbd8b7989&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/3bf66373c66580082c772d0565f1476db76e3eec2c3bcdd4f5c403ab471f75a7?apiKey=052aa92796d54d509e3efd8cbd8b7989&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/3bf66373c66580082c772d0565f1476db76e3eec2c3bcdd4f5c403ab471f75a7?apiKey=052aa92796d54d509e3efd8cbd8b7989&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/3bf66373c66580082c772d0565f1476db76e3eec2c3bcdd4f5c403ab471f75a7?apiKey=052aa92796d54d509e3efd8cbd8b7989&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/3bf66373c66580082c772d0565f1476db76e3eec2c3bcdd4f5c403ab471f75a7?apiKey=052aa92796d54d509e3efd8cbd8b7989&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/3bf66373c66580082c772d0565f1476db76e3eec2c3bcdd4f5c403ab471f75a7?apiKey=052aa92796d54d509e3efd8cbd8b7989&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/3bf66373c66580082c772d0565f1476db76e3eec2c3bcdd4f5c403ab471f75a7?apiKey=052aa92796d54d509e3efd8cbd8b7989&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/3bf66373c66580082c772d0565f1476db76e3eec2c3bcdd4f5c403ab471f75a7?apiKey=052aa92796d54d509e3efd8cbd8b7989&"
+            className="absolute h-full w-full object-cover object-center inset-0"
+            alt=""
+          />
+        </picture>
         <div className="relative text-white text-4xl font-bold max-w-[650px] mt-9 max-md:max-w-full">
           We are one-stop solution for all your heating and plumbing needs
         </div>
