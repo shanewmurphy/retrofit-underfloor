db5e5b51fa5c78da8634dc14f920825ac18b9df6
Shane Murphy <shn.murphy@gmail.com>
2024-01-16 14:18:15 +0000

    Updated youtube video

--- app/components/Video.jsx
+++ app/components/Video.jsx
@@ -2,12 +2,12 @@ export default function Video() {
   return (
     <div>
       <div>
-        <h1>YouTube Video Example</h1>
+        {/* <h1>YouTube Video Example</h1> */}
         <div className="video-responsive">
           <iframe
             width="560"
             height="315"
-            src="https://youtu.be/l3__aCWq9_E?si=uvlW8gpjurKhO5pp"
+            src="https://www.youtube.com/embed/jpCJfYoXdC0?si=bUzz32HdwaCn_K6G"
             title="YouTube video player"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
